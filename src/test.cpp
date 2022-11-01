266615b0caa3a8b785d2f50fa0db2d20c67d101b
Ishan Hegde <ishan.hegde@hotmail.com>
2022-11-02 07:15:12 +0800

    Test of Template function with LU and newton

--- src/test.cpp
+++ src/test.cpp
@@ -1,19 +1,42 @@
-#include "Eigen/Dense"
+#include "Jmle.h"
 #include <iostream>
 
+
+struct function
+    {
+        Eigen::VectorXd operator()(Eigen::VectorXd in )
+            {
+                Eigen::VectorXd out(in.rows());
+
+                for (int i =0;i<in.rows();i++)
+                    {
+                        out(i)=in(i)*in(i)+87*in(i)+0.188;
+                    }
+                return out;
+            }
+    };
 
 int main()
     {
 
+        Eigen::VectorXd x(10);
 
-        Eigen::MatrixXf mat(4,4);
-        mat << 1, 2, 6, 9,
-         3, 1, 7, 2,
-         5, 1, 9.02, 2,
-         3, 1, 75, 17;
+        Eigen::MatrixXd y = Eigen::MatrixXd::Identity(10,10);
 
-        mat=mat.array()-1.0;
+        for (int i=0;i<10;i++)
+            {
+                x(i)=i/18+i*i*0.189+89;
+            }
 
-        std::cout<<mat<<std::endl;
+        function f;
+        bool check;
+        check=true;
+        newt(x,check,f);
+
+        //LUdcmp obj = LUdcmp(y);
+
+
+
+        std::cout<<x<<std::endl;
 
     }
