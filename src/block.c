17c3435d3449bb283e27f81a44b2cc4ad9bcacf6
Ishan Hegde <ishan.hegde@hotmail.com>
2023-09-18 02:51:42 +0800

    Calloc block implimentation

--- src/block.c
+++ src/block.c
@@ -1,5 +1,7 @@
 #include "block.h"
 
+#include <stdio.h>
+#include <stdlib.h>
 
 struct mem_block * alloc_mem_block(size_t size){
 
@@ -19,6 +21,27 @@ struct mem_block * alloc_mem_block(size_t size){
     }
 
     return block;
+}
+
+struct mem_block * calloc_mem_block(size_t size){
+
+    struct mem_block * block = (struct mem_block *) malloc(sizeof(struct mem_block));
+
+    if (block == NULL){
+        fprintf(stderr, "Failed to allocate memory for mem_block structure.\n");
+        return NULL;
+    }
+
+    block->data = (double *) calloc(size,sizeof(double));
+
+    if (block->data == NULL){
+        fprintf(stderr, "Failed to allocate memory for data array.\n");
+        free(block);
+        return NULL;
+    }
+
+    return block;
+
 }
 
 void free_block(struct mem_block * block){
--- src/block.h
+++ src/block.h
@@ -12,6 +12,8 @@ struct mem_block{
 
 struct mem_block * alloc_mem_block(size_t size);
 
+struct mem_block * calloc_mem_block(size_t size);
+
 void free_block(struct mem_block * block);
 
 #endif // _BLOCK_H_
