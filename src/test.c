1546f0bbdb4d712604bca8813285e401607f4578
Ishan Hegde <ishan.hegde@hotmail.com>
2023-09-18 19:25:37 +0800

    Removing depricated configs

--- src/test.c
+++ src/test.c
@@ -7,17 +7,20 @@
 #include <time.h>
 #include <cblas.h>
 #include <lapacke.h>
-
 #include "utils.h"
+#include "panopticon.h"
 
 int main(){
+
+    GLOBAL_TIMER(MILLISECONDS,CLOCK_MONOTONIC_RAW)
 
     srand(time(NULL));
     goto_set_num_threads( 16);
     openblas_set_num_threads( 16);
-    struct matrix * A = alloc_matrix(70,10);
+    omp_set_num_threads(10);
+    struct matrix * A = alloc_matrix(70000,6);
 
-    generate_simulated_data(A,-2,2,0,2);
+    generate_simulated_data(A,-1,1,0,1);
 
     struct dichotomus_model * model =  dichotomus_model_create_alloc(A);
 
@@ -26,11 +29,19 @@ int main(){
     //print_vector(model->person_ability);
     //print_vector(model->item_difficulty);
 
-    for (int i =0; i< 3; i++) {
+
+    for (int i =0; i< 100; i++) {
+        double l = calculate_likelihood(model);
         update_person_parameters(model);
+        //standardize_vector_Zscore(model->person_ability);
         update_item_parameters(model);
+        //standardize_vector_Zscore(model->item_difficulty);
+        
+        printf("%d th iteration: %lf \n",i,l);
     }
+
     //print_vector(model->person_ability);
+    
     print_vector(model->item_difficulty);
 
     //print_vector(model->person_v_total_scores);
