71ff0bc2da62aa494b6c6228f75faecc6598ccc8
Ishan Hegde <ishan.hegde@hotmail.com>
2023-10-09 04:55:29 +0800

    Fixed AVX calls

--- include/vecmath.h
+++ include/vecmath.h
@@ -6,6 +6,10 @@ typedef struct complex_8{
     __m256 imag;
 } complex_8;
 
+typedef struct complex_4{
+    __m256d real;
+    __m256d imag;
+} complex_4;
 
 static inline complex_8 ADD(complex_8 a, complex_8 b){
 
@@ -16,11 +20,29 @@ static inline complex_8 ADD(complex_8 a, complex_8 b){
     return ret;
 }
 
+static inline complex_4 ADD_4(complex_4 a, complex_4 b){
+
+    complex_4 ret;
+    ret.real = _mm256_add_pd(a.real, b.real);
+    ret.imag = _mm256_add_pd(a.imag, b.imag);
+
+    return ret;
+} 
+
 static inline complex_8 SUB(complex_8 a, complex_8 b){
 
     complex_8 ret;
     ret.real = _mm256_sub_ps(a.real, b.real);
     ret.imag = _mm256_sub_ps(a.imag, b.imag);
+
+    return ret;
+}
+
+static inline complex_4 SUB_4(complex_4 a, complex_4 b){
+
+    complex_4 ret;
+    ret.real = _mm256_sub_pd(a.real, b.real);
+    ret.imag = _mm256_sub_pd(a.imag, b.imag);
 
     return ret;
 }
@@ -34,18 +56,61 @@ static inline complex_8 MUL(complex_8 a, complex_8 b){
     return ret;
 }
 
+static inline complex_4 MUL_4(complex_4 a, complex_4 b){
+
+    complex_4 ret;
+    ret.real = _mm256_sub_pd(_mm256_mul_pd(a.real,b.real),_mm256_mul_pd(a.imag,b.imag));
+    ret.imag = _mm256_add_pd(_mm256_mul_pd(a.real,b.imag),_mm256_mul_pd(a.imag,b.real));
+
+    return ret;
+}
+
+static inline complex_4 FMADD_4(complex_4 a, complex_4 b, complex_4 c){
+
+    complex_4 ret;
+    ret.real = _mm256_fmadd_pd(a.real,b.real,c.real);
+    ret.imag = _mm256_fmadd_pd(a.imag,b.imag,c.imag);
+
+    return ret;
+}
+
+static inline complex_4 FMSUB_4(complex_4 a, complex_4 b, complex_4 c){
+
+    complex_4 ret;
+    ret.real = _mm256_fnmadd_pd(a.real,b.real,c.real);
+    ret.imag = _mm256_fnmadd_pd(a.imag,b.imag,c.imag);
+
+    return ret;
+}
+
 static inline complex_8 LOAD(float * restrict reals, float * restrict imags){
 
     complex_8 ret;
-    ret.real = _mm256_load_ps(reals);
-    ret.imag = _mm256_load_ps(imags);
+    ret.real = _mm256_loadu_ps(reals);
+    ret.imag = _mm256_loadu_ps(imags);
+
+    return ret;
+}
+
+static inline complex_4 LOAD_4(double * restrict reals, double * restrict imags){
+
+    complex_4 ret;
+    ret.real = _mm256_load_pd(reals);
+    ret.imag = _mm256_load_pd(imags);
 
     return ret;
 }
 
 static inline void STORE(float * restrict reals, float * restrict imags, complex_8 val){
 
-    _mm256_store_ps(reals, val.real);
-    _mm256_store_ps(imags, val.imag);
+    _mm256_storeu_ps(reals, val.real);
+    _mm256_storeu_ps(imags, val.imag);
+
+}
+
+static inline void STORE_4(double * restrict reals, double * restrict imags, complex_4 val){
+
+    _mm256_store_pd(reals, val.real);
+    _mm256_store_pd(imags, val.imag);
 
 }
