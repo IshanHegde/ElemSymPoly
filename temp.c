cb003c4774c73c96330ef6a7e33cb9271624d4cb
Ishan Hegde <ishan.hegde@hotmail.com>
2023-10-30 08:04:31 +0800

    Minor cleanup

--- temp.c
+++ temp.c
@@ -1,70 +1,46 @@
-for (int k =0; k < n/2; k+=4){
+static void _elem_symm_poly_comp(poly_mul_state_t poly_mul_state, matrix_t poly_matrix, int stride, int n){
 
-    w = LOAD_4(&w_reals[aux_num][k],&w_imags[aux_num][k]);
+    if ( n == 2){
 
-    y_1_k = LOAD_4(&out_reals[k+n/2],&out_imags[k+n/2]);
-    y_0_k = LOAD_4(&out_reals[k],&out_imags[k]);
-    t = MUL_4(w,y_1_k);
-    
-    STORE_4(&out_reals[k],&out_imags[k],ADD_4(y_0_k,t));
-    STORE_4(&out_reals[k+n/2],&out_imags[k+n/2],SUB_4(y_0_k,t));
-
-}
-
-=> 
-n/2 / 8
-
-for (int k =0; k < n/16; k+=4){
-
-    w = LOAD_4(&w_reals[aux_num][k],&w_imags[aux_num][k]);
-
-    y_1_k = LOAD_4(&out_reals[k+n/2],&out_imags[k+n/2]);
-    y_0_k = LOAD_4(&out_reals[k],&out_imags[k]);
-    t = MUL_4(w,y_1_k);
-    
-    STORE_4(&out_reals[k],&out_imags[k],ADD_4(y_0_k,t));
-    STORE_4(&out_reals[k+n/2],&out_imags[k+n/2],SUB_4(y_0_k,t));
-
-}
+        double poly_matrix_0_1 = poly_matrix[0][1];
+        
+        poly_matrix[0][0] = 1;
+        poly_matrix[0][1] = poly_matrix_0_1 + poly_matrix[stride][1]; 
+        poly_matrix[0][2] = poly_matrix_0_1 * poly_matrix[stride][1];
+        poly_matrix[0][3] = 0;
 
 
-for (int k =n/16; k < n/8 ; k+=4){
+    } else if (n == 4){
 
-    w = LOAD_4(&w_reals[aux_num][k],&w_imags[aux_num][k]);
+        _elem_symm_poly_comp(poly_mul_state, poly_matrix, 1, 2);
+        _elem_symm_poly_comp(poly_mul_state, poly_matrix + 2, 1 , 2);
 
-    y_1_k = LOAD_4(&out_reals[k+n/2],&out_imags[k+n/2]);
-    y_0_k = LOAD_4(&out_reals[k],&out_imags[k]);
-    t = MUL_4(w,y_1_k);
-    
-    STORE_4(&out_reals[k],&out_imags[k],ADD_4(y_0_k,t));
-    STORE_4(&out_reals[k+n/2],&out_imags[k+n/2],SUB_4(y_0_k,t));
+        double poly_matrix_0_1 = poly_matrix[0][1];
+        double poly_matrix_0_2 = poly_matrix[0][2];
+        double poly_matrix_0_3 = poly_matrix[0][3];
 
-}
-
-
-for (int k =n/8; k < 3*n/16 ; k+=4){
-
-    w = LOAD_4(&w_reals[aux_num][k],&w_imags[aux_num][k]);
-
-    y_1_k = LOAD_4(&out_reals[k+n/2],&out_imags[k+n/2]);
-    y_0_k = LOAD_4(&out_reals[k],&out_imags[k]);
-    t = MUL_4(w,y_1_k);
-    
-    STORE_4(&out_reals[k],&out_imags[k],ADD_4(y_0_k,t));
-    STORE_4(&out_reals[k+n/2],&out_imags[k+n/2],SUB_4(y_0_k,t));
-
-}
+        poly_matrix[0][0] = 1;
+        poly_matrix[0][1] = poly_matrix_0_1 + poly_matrix[stride][1];
+        poly_matrix[0][2] = poly_matrix_0_1 * poly_matrix[stride][1] + poly_matrix_0_2 + poly_matrix[stride][2];
+        poly_matrix[0][3] = poly_matrix_0_1 * poly_matrix[stride][2] + poly_matrix_0_2 * poly_matrix[stride][1] + poly_matrix_0_3 + poly_matrix[stride][3];
+        poly_matrix[0][4] = poly_matrix_0_1 * poly_matrix[stride][3] + poly_matrix_0_2 * poly_matrix[stride][2] + poly_matrix_0_3 * poly_matrix[stride][1];
+        poly_matrix[0][5] = poly_matrix_0_2 * poly_matrix[stride][3] + poly_matrix_0_3 * poly_matrix[stride][2];
+        poly_matrix[0][6] = poly_matrix_0_3 * poly_matrix[stride][3];
+        poly_matrix[0][7] = 0;
 
 
-for (int k =3*n/16; k < n/4 ; k+=4){
+    } else{
+        _elem_symm_poly_comp(poly_mul_state, poly_matrix, stride/2, n/2);
+        _elem_symm_poly_comp(poly_mul_state, poly_matrix + n/2, stride/2 , n/2);
 
-    w = LOAD_4(&w_reals[aux_num][k],&w_imags[aux_num][k]);
+        for (int i =0 ;i < n; i+=2*stride){
 
-    y_1_k = LOAD_4(&out_reals[k+n/2],&out_imags[k+n/2]);
-    y_0_k = LOAD_4(&out_reals[k],&out_imags[k]);
-    t = MUL_4(w,y_1_k);
-    
-    STORE_4(&out_reals[k],&out_imags[k],ADD_4(y_0_k,t));
-    STORE_4(&out_reals[k+n/2],&out_imags[k+n/2],SUB_4(y_0_k,t));
+            update_polynomial_mul_state(poly_mul_state, poly_matrix[i], poly_matrix[i + stride], n  );
+            array_t result = polynomial_multiply(poly_mul_state);
 
+            memcpy(poly_matrix[i], result, 2*n * sizeof(double));
+            
+        }
+
+    }   
 }
