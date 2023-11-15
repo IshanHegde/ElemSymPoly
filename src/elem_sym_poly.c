5072d1940ba93844a5eafcb0438cd7d1e7f848de
Ishan Hegde <ishan.hegde@hotmail.com>
2023-11-15 20:43:38 +0800

    Added direct Python C interface

--- src/elem_sym_poly.c
+++ src/elem_sym_poly.c
@@ -5,19 +5,110 @@
 #include <elem_sym_poly.h>
 #include <elementary_symmetric_polynomial.h>
 #include <stdlib.h>
-double * compute_elem_sym_poly(double * elements, int N){
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
-    elementary_symmetric_state_t  state = init_elementary_symmetric_state(N);
+#if PY_MAJOR_VERSION < 3
+#error "Requires Python 3"
+#include "stopcompilation"
+#endif
 
-    update_elementary_symmetric_state(state, elements, N);
 
-    double * poly = compute_elementary_symmetric_polynomials(state);
+#define DEFAULT_PRECISION 53
 
-    free_elementary_symmetric_state(state);
+static PyObject * ElemSymPolyError;
 
-    return poly;
-}
+// This assumes it is passed a list of python floats ( C doubles ) of length power of 2
+static PyObject * py_compute_elem_sym_poly(PyObject * Py_UNUSED(self), PyObject *args){
 
-void free_poly(double * poly){
-    free(poly);
+	PyObject * elements_lst;
+	int N;
+	int precision = DEFAULT_PRECISION;
+
+	if (!PyArg_ParseTuple(args, "O!i", &PyList_Type, &elements_lst, &precision)){
+		return NULL;
+	}
+
+	N = (int)PyList_Size(elements_lst);
+
+	double * elements = malloc(sizeof(double) * N);
+
+	for (int i = 0;i< N; i++){
+		PyObject* item = PyList_GetItem(elements_lst, i);
+
+		if (!PyFloat_Check(item) || PyErr_Occurred()){
+			PyErr_SetString(ElemSymPolyError, "List must contain only floats");
+			free(elements);
+			return NULL;
+		}
+		else{
+			elements[i] = PyFloat_AsDouble(item);
+		}
+	}
+
+	elementary_symmetric_state_t  state = init_elementary_symmetric_state(N, precision);
+
+	update_elementary_symmetric_state(state, elements, N);
+
+	double * poly = compute_elementary_symmetric_polynomials(state);
+
+	free_elementary_symmetric_state(state);
+	mpfr_mp_memory_cleanup();
+
+	PyObject * poly_lst = PyList_New(N+1);
+
+	for (int i = 0;i< N+1; i++){
+		PyObject * item = PyFloat_FromDouble(poly[i]);
+
+		if (item == NULL){
+			PyErr_SetString(ElemSymPolyError, "Internal error");
+			free(elements);
+			free(poly);
+			return NULL;
+		}
+
+		PyList_SetItem(poly_lst, i, item);
+	}
+
+	free(poly);
+	free(elements);
+
+	return poly_lst;
+
+};
+
+static PyMethodDef ElemSymPolyMethods[] = {
+		{"compute_elem_sym_poly", py_compute_elem_sym_poly, METH_VARARGS, "Compute the elementary symmetric polynomials"},
+		{NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef ElemSymPolyDef = {
+		PyModuleDef_HEAD_INIT,
+		"elem_sym_poly",
+		NULL,
+		-1,
+		ElemSymPolyMethods,
+};
+
+PyMODINIT_FUNC PyInit_elem_sym_poly(void){
+
+
+	PyObject * module = PyModule_Create(&ElemSymPolyDef);
+
+
+	if (module == NULL){
+		return NULL;
+	}
+
+	ElemSymPolyError = PyErr_NewException("elem_sym_poly.error", NULL, NULL);
+	Py_INCREF(ElemSymPolyError);
+
+	if (PyModule_AddObject(module, "error", ElemSymPolyError ) < 0){
+		Py_DECREF(ElemSymPolyError);
+		Py_CLEAR(ElemSymPolyError);
+		Py_DECREF(module);
+		return NULL;
+	}
+
+	return module;
 }
